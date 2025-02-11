a4fd202ef263d4f4a11233cff0f9f706b3cd2f46
Aysulu Gataullina <aysulu.gataullina@truesolv.com>
2025-02-11 23:54:28 +0300

    final changes

--- frontend/src/services/config.js
+++ frontend/src/services/config.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = 'http://localhost:5150/';
-export const SIGNALR_BASE_URL = 'http://localhost:5150/game-session';
+export const API_BASE_URL = 'http://backend:5271/';
+export const SIGNALR_BASE_URL = 'http://backend:5271/game-session';
